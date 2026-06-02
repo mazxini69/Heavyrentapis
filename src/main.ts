e180ffd37419349d3951fd26da500f533db143a4
mazxini69 <20235740@esen.edu.sv>
2026-06-01 19:39:37 -0600

    implementacion de paso 8

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
